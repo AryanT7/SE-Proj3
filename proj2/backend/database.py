0e953f7a78909f56bbc6e0b42602576cda6ea5c6
Dilip <indkumar8999@gmail.com>
2025-12-06 16:07:01 -0500

    remove dotenv from database.py

--- proj2/backend/database.py
+++ proj2/backend/database.py
@@ -1,7 +1,5 @@
 import mysql.connector
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 # Schema table names
 tables = ['theatres', 'auditoriums', 'seats', 'users', 'staff', 'movies', 'movie_showings',
